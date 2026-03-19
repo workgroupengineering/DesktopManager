1a580ec1354ad25e9abc3e61a2382b8a4f35ae48
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-03-19 09:22:13 +0100

    chore(build): 🔧 restore module publish config examples

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -97,4 +97,8 @@ Invoke-ModuleBuild @invokeModuleBuild -Settings {
 
     New-ConfigurationArtefact -Type Unpacked -Enable -Path "$PSScriptRoot\..\Artefacts\Unpacked" -RequiredModulesPath "$PSScriptRoot\..\Artefacts\Unpacked\Modules"
     New-ConfigurationArtefact -Type Packed -Enable -Path "$PSScriptRoot\..\Artefacts\Packed" -IncludeTagName -ArtefactName 'DesktopManager-PowerShellModule.<TagModuleVersionWithPreRelease>.zip' -ID 'ToGitHub'
+
+    # global options for publishing to github/psgallery
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true -ID 'ToGitHub' -OverwriteTagName 'DesktopManager-PowerShellModule.<TagModuleVersionWithPreRelease>'
 }
